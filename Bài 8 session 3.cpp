9993d4b8ca9dddcd4abcbf23b86fd32b7e909294
steventhefool <hoangduongnampb.2k6@gmail.com>
2024-12-12 09:36:29 +0700

    Add files via upload

--- Bài 8 session 3.cpp
+++ Bài 8 session 3.cpp
@@ -5,11 +5,13 @@ int main (){
 	printf ("nhap vao mot so nguyen gom 4 chu so: ");
 	scanf ("%d", &n);
 	if ((n >999) && (n<10000)){
-		while (n!=0){
-			du = n%10;
-			reversed = reversed*10 + du;
-			n/=10;
-		}
+		reversed += (n%10)*1000;
+		n/=10;
+		reversed += (n%10)*100;
+		n/=10;
+		reversed+= (n%10)*10;
+		n/=10;
+		reversed += n%10;
 		printf ("gia tri nghich dao cua so nguyen da nhap vao la: %d", reversed);
 	}
 	else {
